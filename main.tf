7cad22a876f6efa5710b22a1c521d5b8b1c8fb0c
Jimoh Meyer <98897712+jimohmeyer@users.noreply.github.com>
2023-09-06 11:33:34 +0200

    clean up of old sg

--- main.tf
+++ main.tf
@@ -22,8 +22,6 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  #include the security group that I defined further down
-  #vpc_security_group_ids = [aws_security_group.blog.id]
   vpc_security_group_ids = [module.blog_module_sg.security_group_id]
 
   tags = {
@@ -36,7 +34,7 @@ resource "aws_instance" "blog" {
 module "blog_module_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name = "blog_new"
+  name = "blog"
 
   vpc_id = data.aws_vpc.default.id
 
@@ -45,45 +43,4 @@ module "blog_module_sg" {
 
   egress_rules = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
-}
-
-
-
-
-#security group definition (Video Security group)
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow http and https in and everything out"
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type         = "ingress"
-  from_port    = 80
-  to_port      = 80
-  protocol     = "tcp"
-  cidr_blocks  = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type         = "ingress"
-  from_port    = 443
-  to_port      = 443
-  protocol     = "tcp"
-  cidr_blocks  = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_traffic_out" {
-  type         = "egress"
-  from_port    = 0
-  to_port      = 0
-  protocol     = "-1"
-  cidr_blocks  = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
 }
