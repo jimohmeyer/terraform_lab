fa4fad52ccbd5e978270c0393ee36492b2eaa8f1
Jimoh Meyer <98897712+jimohmeyer@users.noreply.github.com>
2023-09-06 11:28:58 +0200

    Use module for security group

--- main.tf
+++ main.tf
@@ -23,12 +23,31 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
   #include the security group that I defined further down
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  #vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_module_sg.security_group_id]
 
   tags = {
     Name = "Terraform Test"
   }
 }
+
+
+
+module "blog_module_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks  = ["0.0.0.0/0"]
+
+  egress_rules = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
+}
+
+
 
 
 #security group definition (Video Security group)
