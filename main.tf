ab98cb3abda2fe8d4ee666506b6a5c43b529ae3f
Jimoh Meyer <98897712+jimohmeyer@users.noreply.github.com>
2023-09-06 10:43:02 +0200

    Create security group

--- main.tf
+++ main.tf
@@ -14,11 +14,57 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  #include the security group that I defined further down
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
     Name = "Terraform Test"
   }
+}
+
+
+#security group definition (Video Security group)
+resource "aws_security_group" "blog" {
+  name        = "blog"
+  description = "Allow http and https in and everything out"
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type         = "ingress"
+  from_port    = 80
+  to_port      = 80
+  protocol     = "tcp"
+  cidr_blocks  = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type         = "ingress"
+  from_port    = 443
+  to_port      = 443
+  protocol     = "tcp"
+  cidr_blocks  = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_traffic_out" {
+  type         = "egress "
+  from_port    = 0
+  to_port      = 0
+  protocol     = "-1"
+  cidr_blocks  = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
 }
