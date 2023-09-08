04b598ac7123fd9caba7a9b583de284ced3dcd71
Jimoh Meyer <98897712+jimohmeyer@users.noreply.github.com>
2023-09-08 16:02:59 +0200

    revert

--- main.tf
+++ main.tf
@@ -15,14 +15,13 @@ data "aws_ami" "app_ami" {
 }
 
 #Copied from the docs of the terraform registry module "VPC"
-
-module "blog_vpc" {
+module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
+  name = "dev-env"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
@@ -51,7 +50,7 @@ module "blog_module_sg" {
   version = "5.1.0"
   name = "blog"
 
-  vpc_id  = module.blog_vpc.vpc_id
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks  = ["0.0.0.0/0"]
